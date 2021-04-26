b2589576849d1c65701662c251dc8ca5f73a97a7
NikitaDombrovsky <nikita.nikita55@mail.ru>
2021-04-26 17:54:10 +0300

    Hello there

---
+++ app/src/main/java/com/example/superfit/one_autorization_avtivity.java
@@ -0,0 +1,15 @@
+package com.example.superfit;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class one_autorization_avtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one_autorization_screen);
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/example/superfit/registration_activity.java
@@ -0,0 +1,16 @@
+package com.example.superfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_screen);
+        getSupportActionBar().hide();
+    }
+    
+}
