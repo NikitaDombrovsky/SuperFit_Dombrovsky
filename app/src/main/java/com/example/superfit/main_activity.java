ac4f1717ad6b84d417b940772233d3e1a3199ef7
NikitaDombrovsky <nikita.nikita55@mail.ru>
2021-04-28 17:25:09 +0300

    Третий рабочий день

--- app/src/main/java/com/example/superfit/main_activity.java
+++ app/src/main/java/com/example/superfit/main_activity.java
@@ -1,6 +1,8 @@
 package com.example.superfit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,9 @@ public class main_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+    }
+    public void Sign_out_main(View view){
+        Intent intent = new Intent(main_activity.this, autorization_activity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/superfit/registration_activity.java
+++ app/src/main/java/com/example/superfit/registration_activity.java
@@ -2,7 +2,9 @@ package com.example.superfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registration_activity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class registration_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_screen);
     }
-    
+    public void Sign_up_reg(View view){
+        Intent intent = new Intent(registration_activity.this, main_activity.class);
+        startActivity(intent);
+    }
+    public void Sign_in_reg(View view){
+        Intent intent = new Intent(registration_activity.this, autorization_activity.class);
+        startActivity(intent);
+    }
 }
