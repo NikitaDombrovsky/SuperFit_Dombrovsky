6f87b627865a9a24e56f4c9195fc97dca69e1a9e
NikitaDombrovsky <nikita.nikita55@mail.ru>
2021-04-29 15:30:34 +0300

    Четвертый день

--- app/src/main/java/com/example/superfit/Based.java
+++ app/src/main/java/com/example/superfit/Based.java
@@ -1,4 +1,32 @@
 package com.example.superfit;
 
-public class Based {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class Based extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "base";
+    public static final String TABLE_CONTACTS = "based";
+
+    public static final String ID ="_id";
+    public static final String USERNAME ="username";
+    public static final String EMAIL ="@email.com";
+    public static final String CODE ="1234";
+
+
+    public Based(Context context, String name, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
